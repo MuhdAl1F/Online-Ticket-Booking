3187e4d34d0239f7f5e4237c54f43a47759c468e
MuhdAl1F <71406524+MuhdAl1F@users.noreply.github.com>
2021-06-24 19:14:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <center>
+        <div class="topnav">
+            <a class="active" href="index.php?menu=#home">Home</a>
+            <a href="daftar.php">Daftar Tiket</a>
+            <a href="logout.php">Logout</a>
+        </div>
+
+        <h3>Name System : Online Ticket Booking (OTB)</h3><br>
+        <br>Nama Programmer :<a href="staff">Staff(Alif Azni)</a>
+    </center>
+    
+</body>
+</html>
